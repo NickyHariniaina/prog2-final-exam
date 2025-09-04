9ee60465a974ccb4788e7adc01e84def7b95eac5
NickyHariniaina <nickyharini1na@gmail.com>
2025-09-04 11:39:41 +0300

    feat: add better test case to cover each case

--- src/test/java/NickyHariniaina/FeesTest.java
+++ src/test/java/NickyHariniaina/FeesTest.java
@@ -8,6 +8,7 @@ import java.time.format.DateTimeFormatter;
 
 import org.junit.jupiter.api.Test;
 
+import NickyHariniaina.FeesHistory.BankPaiment;
 import NickyHariniaina.FeesHistory.ClassGroup;
 import NickyHariniaina.FeesHistory.Fees;
 import NickyHariniaina.FeesHistory.FeesStatus;
@@ -18,20 +19,64 @@ import NickyHariniaina.FeesHistory.Student;
 public class FeesTest {
 
   @Test
-  public void should_return_fees_status_ok() {
+  public void should_return_late_fees_status_ok() {
 
     Student me = new Student("STD24033", "Nicky", "Hariniaina", LocalDate.of(2024, 9, 18),
         new ClassGroup("001", "K2"));
 
     Fees L1Fees = new Fees("001", "First year fees", 2_000_000.00, 0.00, LocalDate.of(2025, 8, 30), me,
         FeesStatus.IN_PROGRESS);
+    Paiment firstPaiment = new MobilePaiment("001", 1_000_000.00, Instant.parse("2025-04-12T00:00:00Z"), "MP001");
+    Paiment secondPaiment = new BankPaiment("002", 3_000_000, Instant.parse("2025-09-23T00:00:00Z"), "BK004");
 
-    Paiment myPaiment = new MobilePaiment("001", 1_000_000.00, Instant.parse("2024-04-12T00:00:00Z"), "MP001");
+    L1Fees.payFees(firstPaiment);
 
-    L1Fees.payFees(myPaiment);
-
-    assertEquals(FeesStatus.IN_PROGRESS, L1Fees.getFeesStatusAt(Instant.parse("2025-04-13T23:58:11Z")));
     assertEquals(FeesStatus.LATE, L1Fees.getFeesStatusAt(Instant.parse("2025-10-12T00:00:00Z")));
   }
 
+  public void should_return_in_progress_fees_status_ok() {
+
+    Student me = new Student("STD24033", "Nicky", "Hariniaina", LocalDate.of(2024, 9, 18),
+        new ClassGroup("001", "K2"));
+
+    Fees L1Fees = new Fees("001", "First year fees", 2_000_000.00, 0.00, LocalDate.of(2025, 8, 30), me,
+        FeesStatus.IN_PROGRESS);
+    Paiment firstPaiment = new MobilePaiment("001", 1_000_000.00, Instant.parse("2025-04-12T00:00:00Z"), "MP001");
+    Paiment secondPaiment = new BankPaiment("002", 3_000_000, Instant.parse("2025-09-23T00:00:00Z"), "BK004");
+
+    L1Fees.payFees(firstPaiment);
+
+    assertEquals(FeesStatus.IN_PROGRESS, L1Fees.getFeesStatusAt(Instant.parse("2025-05-12T00:00:00Z")));
+
+  }
+
+  public void should_return_paid_fees_status_ok() {
+
+    Student me = new Student("STD24033", "Nicky", "Hariniaina", LocalDate.of(2024, 9, 18),
+        new ClassGroup("001", "K2"));
+
+    Fees L1Fees = new Fees("001", "First year fees", 2_000_000.00, 0.00, LocalDate.of(2025, 8, 30), me,
+        FeesStatus.IN_PROGRESS);
+    Paiment firstPaiment = new MobilePaiment("001", 2_000_000.00, Instant.parse("2025-04-12T00:00:00Z"), "MP001");
+
+    L1Fees.payFees(firstPaiment);
+
+    assertEquals(FeesStatus.PAID, L1Fees.getFeesStatusAt(Instant.parse("2025-05-12T00:00:00Z")));
+
+  }
+
+  public void should_return_overpaid_fees_status_ok() {
+
+    Student me = new Student("STD24033", "Nicky", "Hariniaina", LocalDate.of(2024, 9, 18),
+        new ClassGroup("001", "K2"));
+
+    Fees L1Fees = new Fees("001", "First year fees", 2_000_000.00, 0.00, LocalDate.of(2025, 8, 30), me,
+        FeesStatus.IN_PROGRESS);
+    Paiment firstPaiment = new MobilePaiment("001", 4_000_000.00, Instant.parse("2025-04-12T00:00:00Z"), "MP001");
+
+    L1Fees.payFees(firstPaiment);
+
+    assertEquals(FeesStatus.OVERPAID, L1Fees.getFeesStatusAt(Instant.parse("2025-05-12T00:00:00Z")));
+
+  }
 }
